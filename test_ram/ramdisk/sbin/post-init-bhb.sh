bc6f171194f17821d1603dd4c937cf93e897ce9b
bhb27 <fglfgl27@gmail.com>
2016-09-28 17:35:57 -0300

    Add test ramdisk
    Signed-off-by: bhb27 <fglfgl27@gmail.com>

---
+++ test_ram/ramdisk/sbin/post-init-bhb.sh
@@ -0,0 +1,49 @@
+#!/system/bin/sh
+#script init pull from neobuddy89 github
+
+# Mount root as RW to apply tweaks and settings
+mount -o remount,rw /
+mount -o rw,remount /system
+
+# Give permissions to execute
+chmod -R 777 /tmp/
+chmod 6755 /sbin/*
+chmod 6755 /system/xbin/*
+
+# Make tmp folder
+if [ -e /tmp ]; then
+	echo "tmp already exist"
+else
+mkdir /tmp
+fi
+
+#enable, disable and tweak some features of the kernel by default for better performance vs battery
+
+# CPU - Disable hotplug boost
+echo 0 > /sys/module/cpu_boost/parameters/hotplug_boost
+
+# CPU - set max clock to stock value
+chmod 644 /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+echo 2649600 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+echo 1 > /sys/devices/system/cpu/cpu1/online
+chmod 644 /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
+echo 2649600 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
+echo 1 > /sys/devices/system/cpu/cpu2/online
+chmod 644 /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
+echo 2649600 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
+echo 1 > /sys/devices/system/cpu/cpu3/online
+chmod 644 /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
+echo 2649600 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
+
+# GPU max clock to stock value, enable adreno_idler
+echo 600000000 > /sys/devices/fdb00000.qcom,kgsl-3d0/kgsl/kgsl-3d0/max_gpuclk
+echo Y > /sys/module/adreno_idler/parameters/adreno_idler_active
+
+# Ena adaptive lmk Tune LMK
+echo 1 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
+echo 52992 > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
+echo 4096,8192,16640,29184,47104,52224 > /sys/module/lowmemorykiller/parameters/minfree
+
+echo "Post init Kernel Boot initiated on $(date)" >> /tmp/bootcheck.txt
+
+exit
---
+++ test_ram/ramdisk/sbin/post-init-rr.sh
@@ -0,0 +1,49 @@
+#!/system/bin/sh
+#script init pull from neobuddy89 github
+
+# Mount root as RW to apply tweaks and settings
+mount -o remount,rw /
+mount -o rw,remount /system
+
+# Give permissions to execute
+chmod -R 777 /tmp/
+chmod 6755 /sbin/*
+chmod 6755 /system/xbin/*
+
+# give su root:root to adb su work
+chown root:root /system/xbin/su
+
+# Make tmp folder
+if [ -e /tmp ]; then
+	echo "tmp already exist"
+else
+mkdir /tmp
+fi
+
+# only present on RR this need to be 755 to execute...
+if [ -e /system/app/Adaway/lib/arm/libblank_webserver_exec.so ]; then
+	chmod 755 /system/app/Adaway/lib/arm/libblank_webserver_exec.so
+fi
+
+if [ -e /system/app/Adaway/lib/arm/libtcpdump_exec.so ]; then
+	chmod 755 /system/app/Adaway/lib/arm/libtcpdump_exec.so
+fi
+
+fsgid=`getprop ro.boot.fsg-id`;
+device=`getprop ro.boot.hardware.sku`
+
+if  [ "$device" == XT1225 ] ||  [ "$fsgid" == emea ] || [ "$fsgid" == singlela ]; then
+	# stop IMS services Not need for others then VZW users
+	stop imsqmidaemon;
+	stop imsdatadaemon;
+	setprop net.lte.volte_call_capable false
+	echo "services stop okay device = $device fsgid = $fsgid" >> /tmp/bootcheck.txt;
+
+else
+	echo "services not stoped for device = $device fsgid = $fsgid" >> /tmp/bootcheck.txt;
+fi;
+
+echo "post-init-ROM Boot initiated on $(date)" >> /tmp/bootcheck.txt
+
+exit
+
